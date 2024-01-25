9afc105c25e5525d8f904e3146abfd77df322db2
Peter <peter.kacurik@proton.me>
2024-01-25 15:07:42 +0100

    google auth set up

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,18 @@
+import { onLog } from "firebase/app";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
   return (
     <>
-      <p>Hello from sSignIn</p>
+      <p>Sign in page</p>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </>
   );
 };
