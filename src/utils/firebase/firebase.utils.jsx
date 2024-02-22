0dfc8b4a8224ed5150abd270528d590cb2634947
Peter <142713164+KacpeSK@users.noreply.github.com>
2024-02-22 11:46:45 +0100

    additinal tweaks to deploy test

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -24,13 +24,26 @@ import {
 
 import.meta.env.VITE_apiKey;
 
-const firebaseConfigENV = {
+//Use during dev
+
+/* const firebaseConfigENV = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId,
+};*/
+
+//Use in deployTest
+
+const firebaseConfigENV = {
+  apiKey: process.env.VITE_apiKey,
+  authDomain: process.env.VITE_authDomain,
+  projectId: process.env.VITE_projectId,
+  storageBucket: process.env.VITE_storageBucket,
+  messagingSenderId: process.env.VITE_messagingSenderId,
+  appId: process.env.VITE_appId,
 };
 
 const firebaseConfig = firebaseConfigENV;
