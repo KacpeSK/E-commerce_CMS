70ad2a9dd99cb43c939ed0ba28d832e9365d08a5
Peter <142713164+KacpeSK@users.noreply.github.com>
2025-03-09 21:58:10 +0100

    feat: add react-router home

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,19 @@
 /* eslint-disable */
 // @ts-nocheck
+import { Routes, Route } from "react-router";
 
-import "./App.css";
-import Directory from "./components/directory/directory.component";
+import "./App.scss";
+import Home from "./routes/home/home.components";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
     <section>
-      <Directory categories={categories} />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        ></Route>
+      </Routes>
     </section>
   );
 };
