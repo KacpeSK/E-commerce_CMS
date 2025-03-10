d9f21d5bf28a27d906b4d5e2a3280c3e075a2d20
Peter <142713164+KacpeSK@users.noreply.github.com>
2025-03-10 18:17:53 +0100

    feat: navigation and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable */
 // @ts-nocheck
 import { Routes, Route } from "react-router";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 import "./App.scss";
 import Home from "./routes/home/home.components";
@@ -11,12 +13,33 @@ const App = () => {
       <Routes>
         <Route
           path="/"
-          element={<Home />}
-        ></Route>
+          element={<Navigation />}
+        >
+          <Route
+            index
+            element={<Home />}
+          />
+          <Route
+            path="shop"
+            element={<Shop />}
+          />
+          <Route
+            path="signIn"
+            element={<SignIn />}
+          />
+        </Route>
       </Routes>
     </section>
   );
 };
 
 export default App;
+
+const Shop = () => {
+  return (
+    <>
+      <p>I am SHOP :</p>
+    </>
+  );
+};
 
